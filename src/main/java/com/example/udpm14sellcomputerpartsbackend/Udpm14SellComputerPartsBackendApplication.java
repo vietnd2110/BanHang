c74f4ed5c3d32e85a17a6654ba9e2648681bf976
vietnd2110 <ducviet2101nb@gmail.com>
2022-09-06 21:31:06 +0700

    createbase

---
+++ src/main/java/com/example/udpm14sellcomputerpartsbackend/Udpm14SellComputerPartsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.udpm14sellcomputerpartsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Udpm14SellComputerPartsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Udpm14SellComputerPartsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/udpm14sellcomputerpartsbackend/Udpm14SellComputerPartsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.udpm14sellcomputerpartsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Udpm14SellComputerPartsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
