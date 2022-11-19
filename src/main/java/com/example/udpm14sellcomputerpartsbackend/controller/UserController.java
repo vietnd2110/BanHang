2e691c249e86ecb8befdcfdd378158a0bc49f2ca
duchvph17480 <hoangvietduc34hd@gmail.com>
2022-11-19 22:27:55 +0700

    duchv-filter-user

--- src/main/java/com/example/udpm14sellcomputerpartsbackend/controller/UserController.java
+++ src/main/java/com/example/udpm14sellcomputerpartsbackend/controller/UserController.java
@@ -22,6 +22,27 @@ public class UserController {
     public ResponseEntity<?> findByUserId(@PathVariable("id") Long id){
         return ResponseEntity.ok(DefaultResponse.success(userService.findById(id)));
     }
+    @GetMapping("/staff")
+    public ResponseEntity<?> findByStaff(
+            @RequestParam(value = "page",defaultValue = "0") Integer pageSize,
+            @RequestParam(value = "page-number", required = false) Integer pageNumber
+    ){
+        return ResponseEntity.ok(DefaultResponse.success(userService.findAllByStaff(pageSize, pageNumber)));
+    }
+    @GetMapping("/admin")
+    public ResponseEntity<?> findByAdmin(
+            @RequestParam(value = "page",defaultValue = "0") Integer pageSize,
+            @RequestParam(value = "page-number", required = false) Integer pageNumber
+    ){
+        return ResponseEntity.ok(DefaultResponse.success(userService.findAllByAdmin(pageSize, pageNumber)));
+    }
+    @GetMapping("/customer")
+    public ResponseEntity<?> findByCustomer(
+            @RequestParam(value = "page",defaultValue = "0") Integer pageSize,
+            @RequestParam(value = "page-number", required = false) Integer pageNumber
+    ){
+        return ResponseEntity.ok(DefaultResponse.success(userService.findAllByCustomer(pageSize, pageNumber)));
+    }
 
 
     @PostMapping(value = "/update-image/{id}",consumes = "multipart/form-data")
