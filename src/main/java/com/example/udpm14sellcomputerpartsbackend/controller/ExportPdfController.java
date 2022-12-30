b994ab79a06321122897ececdba76491cad6ad60
Hoang Viet Duc <duchv1@hybrid-technologies.vn>
2022-12-30 08:58:00 +0700

    add:export order

--- src/main/java/com/example/udpm14sellcomputerpartsbackend/controller/ExportPdfController.java
+++ src/main/java/com/example/udpm14sellcomputerpartsbackend/controller/ExportPdfController.java
@@ -1,8 +1,10 @@
 package com.example.udpm14sellcomputerpartsbackend.controller;
 
 import com.example.udpm14sellcomputerpartsbackend.service.ExportService;
+import com.example.udpm14sellcomputerpartsbackend.service.impl.ExportServiceImpl;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.dom4j.DocumentException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
@@ -10,7 +12,12 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @CrossOrigin(origins = "*")
 @RestController
@@ -28,8 +35,10 @@ public class ExportPdfController {
     @GetMapping("order/{id}")
     public ResponseEntity<?> exportPdfOrder(@PathVariable("id") Long idOrder) {
         ByteArrayInputStream byteArrayInputStream = exportService.exportPdfOrder(idOrder);
+        DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
+        String currentDateTime = dateFormatter.format(new Date());
         HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.add("Content", "inline; filename:order.pdf");
+        httpHeaders.add("Content-Disposition", "inline; filename=order"+idOrder+"_"+currentDateTime+".pdf");
 
         return ResponseEntity.ok().headers(httpHeaders).contentType(MediaType.APPLICATION_PDF).body(new InputStreamResource(byteArrayInputStream));
     }
