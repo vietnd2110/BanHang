8c0e9258b03f32c3eb9ff68c0079e5568d02c53c
duchvph17480 <hoangvietduc34hd@gmail.com>
2023-01-13 20:21:33 +0700

    add:thong ke

--- src/main/java/com/example/udpm14sellcomputerpartsbackend/service/impl/StatisticalServiceImpl.java
+++ src/main/java/com/example/udpm14sellcomputerpartsbackend/service/impl/StatisticalServiceImpl.java
@@ -1,8 +1,9 @@
 package com.example.udpm14sellcomputerpartsbackend.service.impl;
 
+import com.example.udpm14sellcomputerpartsbackend.contants.OrderStatusEnum;
 import com.example.udpm14sellcomputerpartsbackend.model.dto.thongKe.StatisticalDto;
-import com.example.udpm14sellcomputerpartsbackend.model.dto.thongKe.ThongKeDto;
 import com.example.udpm14sellcomputerpartsbackend.model.dto.thongKe.ThongKeThangVaNamDto;
+import com.example.udpm14sellcomputerpartsbackend.model.dto.thongKe.ThongKeTrangThaiDonHang;
 import com.example.udpm14sellcomputerpartsbackend.repository.OrderRepository;
 import com.example.udpm14sellcomputerpartsbackend.service.StatisticalService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,12 +28,15 @@ public class StatisticalServiceImpl implements StatisticalService {
     }
 
     @Override
-    public List<StatisticalDto> listThongKeTheoNam(Integer year) {
-        return orderRepository.listHoaDonTheoNam(year);
-    }
+    public ThongKeTrangThaiDonHang thongKeTrangThaiDonHang() {
+        ThongKeTrangThaiDonHang thongKeTrangThaiDonHang = new ThongKeTrangThaiDonHang();
+        thongKeTrangThaiDonHang.setSoDonChoXacNhan(orderRepository.thongKeTrangThaiDonHang(OrderStatusEnum.CHOXACNHAN));
+        thongKeTrangThaiDonHang.setSoDonDangXuLy(orderRepository.thongKeTrangThaiDonHang(OrderStatusEnum.DANGXULY));
+        thongKeTrangThaiDonHang.setSoDonDangVanChuyen(orderRepository.thongKeTrangThaiDonHang(OrderStatusEnum.DANGVANCHUYEN));
+        thongKeTrangThaiDonHang.setSoDonDaGiao(orderRepository.thongKeTrangThaiDonHang(OrderStatusEnum.DAGIAO));
+        thongKeTrangThaiDonHang.setSoDonDaHuy(orderRepository.thongKeTrangThaiDonHang(OrderStatusEnum.DAHUY));
+        thongKeTrangThaiDonHang.setSoDonDaHoanThanh(orderRepository.thongKeTrangThaiDonHang(OrderStatusEnum.DAHOANTHANH));
 
-    @Override
-    public List<ThongKeDto> listThongKeTheoThangVaNam(Integer year, Integer month) {
-        return orderRepository.listHoaDonTheoThang(year, month);
+        return thongKeTrangThaiDonHang;
     }
 }
